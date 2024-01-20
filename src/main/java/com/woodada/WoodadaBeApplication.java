0a60d53c199af5f9749dac2d2f7f5a542b514e38
Jaehong Choe <qnalsfj@gmail.com>
2024-01-20 15:01:08 +0900

    Init: 프로젝트 초기화

---
+++ src/main/java/com/woodada/WoodadaBeApplication.java
@@ -0,0 +1,13 @@
+package com.woodada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodadaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodadaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woodada/WoodadaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodadaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
