9b9ffc6202c791033db6937f683941ee2bdd8e52
Jaehong Choe <qnalsfj@gmail.com>
2024-02-11 12:15:34 +0900

    [Feat] #12 공통 응답 클래스 추가

--- src/main/java/com/woodada/common/auth/adapter/in/OAuth2LoginController.java
+++ src/main/java/com/woodada/common/auth/adapter/in/OAuth2LoginController.java
@@ -4,11 +4,13 @@ import com.woodada.common.auth.adapter.in.response.OAuth2LoginResponse;
 import com.woodada.common.auth.application.port.in.OAuth2LoginUseCase;
 import com.woodada.common.auth.domain.ProviderType;
 import com.woodada.common.auth.domain.Token;
+import com.woodada.common.support.ApiResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,7 @@ public class OAuth2LoginController {
     }
 
     @GetMapping("/{providerType}")
-    public ResponseEntity<OAuth2LoginResponse> login(@PathVariable("providerType") String providerType, @RequestParam("code") String code) {
+    public ResponseEntity<ApiResponse<OAuth2LoginResponse>> login(@PathVariable("providerType") String providerType, @RequestParam("code") String code) {
         final Token token = oAuth2LoginUseCase.login(ProviderType.valueOf(providerType.toUpperCase()), code);
 
         final ResponseCookie refreshTokenCookie = ResponseCookie.from(Token.REFRESH_TOKEN_COOKIE_NAME, token.refreshToken())
@@ -33,9 +35,10 @@ public class OAuth2LoginController {
             .path("/")
             .build();
 
+        final ApiResponse<OAuth2LoginResponse> response = ApiResponse.success(new OAuth2LoginResponse(token.accessToken()));
         return ResponseEntity.ok()
             .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
-            .body(new OAuth2LoginResponse(token.accessToken()));
+            .body(response);
     }
 }
 
--- src/main/java/com/woodada/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/woodada/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.woodada.common.exception;
 
+import com.woodada.common.support.ApiResponse;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,8 +18,8 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResponse handleIllegalArgumentException(final IllegalArgumentException e) {
-        return ErrorResponse.badRequest(e);
+    public ApiResponse<ErrorResponse> handleIllegalArgumentException(final IllegalArgumentException e) {
+        return ApiResponse.error(ErrorResponse.badRequest(e));
     }
 
     /**
@@ -26,8 +27,8 @@ public class GlobalExceptionHandler {
      */
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ErrorResponse handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
-        return ErrorResponse.methodNotAllowed(e);
+    public ApiResponse<ErrorResponse> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+        return ApiResponse.error(ErrorResponse.methodNotAllowed(e));
     }
 
     /**
@@ -35,8 +36,8 @@ public class GlobalExceptionHandler {
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ErrorResponse handleHttpMessageNotReadableException(final HttpMessageNotReadableException e) {
-        return ErrorResponse.badRequest(e);
+    public ApiResponse<ErrorResponse> handleHttpMessageNotReadableException(final HttpMessageNotReadableException e) {
+        return ApiResponse.error(ErrorResponse.badRequest(e));
     }
 
     /**
@@ -44,8 +45,8 @@ public class GlobalExceptionHandler {
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        return ErrorResponse.badRequest(e);
+    public ApiResponse<ErrorResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return ApiResponse.error(ErrorResponse.badRequest(e));
     }
 
     /**
@@ -53,18 +54,16 @@ public class GlobalExceptionHandler {
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
-    public ErrorResponse handleConstraintViolationException(final ConstraintViolationException e) {
-
-        return ErrorResponse.badRequest(e);
+    public ApiResponse<ErrorResponse> handleConstraintViolationException(final ConstraintViolationException e) {
+        return ApiResponse.error(ErrorResponse.badRequest(e));
     }
 
     /**
      * 비즈니스 예외
      */
     @ExceptionHandler(WddException.class)
-    public ResponseEntity<ErrorResponse> handleWddException(final WddException e) {
-        final ErrorResponse response = ErrorResponse.businessError(e);
-
+    public ResponseEntity<ApiResponse<ErrorResponse>> handleWddException(final WddException e) {
+        final ApiResponse<ErrorResponse> response = ApiResponse.error(ErrorResponse.businessError(e));
         return ResponseEntity.status(e.getStatus())
             .body(response);
     }
