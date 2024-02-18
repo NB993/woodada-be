ace016ab66f71ac20a855da36ba330db971bfae4
Jaehong Choe <qnalsfj@gmail.com>
2024-02-18 18:53:23 +0900

    [Feat] #15 origin 정보 주입 방식 변경
    - 의존성 주입 방식 통일
    interceptor를 주입하려고 하는데 @ConfigurationProperties와
    생성자 바인딩 방식이 충돌하여 오류 발생.

--- src/main/java/com/woodada/common/config/WebConfig.java
+++ src/main/java/com/woodada/common/config/WebConfig.java
@@ -1,25 +1,25 @@
 package com.woodada.common.config;
 
 import java.util.List;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@ConfigurationProperties("cors")
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    private List<String> origins;
+    private final CorsProperties corsProperties;
 
-    public void setOrigins(final List<String> origins) {
-        this.origins = origins;
+    public WebConfig(final CorsProperties corsProperties, final AuthInterceptor authInterceptor) {
+        this.corsProperties = corsProperties;
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        final List<String> origins = corsProperties.getOrigins();
+
         registry.addMapping("/**")
             .allowedOrigins(origins.toArray(String[]::new))
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
