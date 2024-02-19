b7f9dc90ba1c13415e2827f1c49c97af513b841a
Jaehong Choe <qnalsfj@gmail.com>
2024-02-19 20:40:10 +0900

    [Feat, Test] MemberAdapter의 GetMeUseCase 의존 제거
    Related to: #15 #22

--- src/main/java/com/woodada/core/member/adapter/in/MemberAdapter.java
+++ src/main/java/com/woodada/core/member/adapter/in/MemberAdapter.java
@@ -3,8 +3,6 @@ package com.woodada.core.member.adapter.in;
 import com.woodada.common.auth.argument_resolver.WddMember;
 import com.woodada.common.support.ApiResponse;
 import com.woodada.core.member.adapter.in.response.MeResponse;
-import com.woodada.core.member.application.port.in.GetMeUseCase;
-import com.woodada.core.member.domain.WddMe;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberAdapter {
 
-    private final GetMeUseCase getMeUseCase;
-
-    public MemberAdapter(final GetMeUseCase getMeUseCase) {
-        this.getMeUseCase = getMeUseCase;
-    }
-
     @GetMapping("/me")
     ResponseEntity<ApiResponse<MeResponse>> getMe(final WddMember wddMember) {
-        final WddMe wddMe = getMeUseCase.getMe(wddMember);
-        final MeResponse meResponse = new MeResponse(wddMe.id(), wddMe.email(), wddMe.name(), wddMe.profileUrl(), wddMe.role());
+        final MeResponse meResponse = new MeResponse(wddMember.getId(), wddMember.getEmail(), wddMember.getName(), wddMember.getProfileUrl(), wddMember.getRole());
 
         final ApiResponse<MeResponse> response = ApiResponse.success(meResponse);
         return ResponseEntity.ok()
--- src/test/java/com/woodada/core/member/adapter/in/MemberAdapterTest.java
+++ src/test/java/com/woodada/core/member/adapter/in/MemberAdapterTest.java
@@ -1,39 +1,28 @@
 package com.woodada.core.member.adapter.in;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 import com.woodada.common.auth.argument_resolver.MemberHelper;
 import com.woodada.common.auth.argument_resolver.WddMember;
-import com.woodada.common.auth.domain.Deleted;
-import com.woodada.common.auth.domain.Member;
 import com.woodada.common.auth.domain.UserRole;
 import com.woodada.common.support.ApiResponse;
 import com.woodada.common.support.ResultCode;
 import com.woodada.core.member.adapter.in.response.MeResponse;
-import com.woodada.core.member.application.port.in.GetMeUseCase;
-import com.woodada.core.member.domain.WddMe;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @DisplayName("[unit test] MemberAdapter 단위 테스트")
-@ExtendWith(MockitoExtension.class)
 class MemberAdapterTest {
 
     private MemberAdapter memberAdapter;
 
-    @Mock private GetMeUseCase getMeUseCase;
-
     @BeforeEach
     void setUp() {
-        memberAdapter = new MemberAdapter(getMeUseCase);
+        memberAdapter = new MemberAdapter();
     }
 
     @DisplayName("getMe 핸들러에 WddMember를 전달받으면 MeResponse를 리턴한다.")
@@ -41,10 +30,6 @@ class MemberAdapterTest {
     void when_get_wdd_member_then_return_me_response() {
         //given
         final WddMember wddMember = getWddMember();
-        final Member member = getMember(wddMember);
-
-        when(getMeUseCase.getMe(wddMember))
-            .thenReturn(new WddMe(member.getId(), member.getEmail(), member.getName(), member.getProfileUrl(), member.getRole()));
 
         //when
             ResponseEntity<ApiResponse<MeResponse>> responseEntity = memberAdapter.getMe(wddMember);
@@ -71,17 +56,6 @@ class MemberAdapterTest {
             "테스트유저",
             "test_profile_url",
             UserRole.NORMAL
-        );
-    }
-
-    private static Member getMember(WddMember wddMember) {
-        return MemberHelper.createMember(
-            wddMember.getId(),
-            wddMember.getEmail(),
-            wddMember.getName(),
-            wddMember.getProfileUrl(),
-            wddMember.getRole(),
-            Deleted.FALSE
         );
     }
 }
