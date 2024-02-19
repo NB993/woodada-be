caa8fae3fe8a9f3dbcb47b184f3433e7a052df29
Jaehong Choe <qnalsfj@gmail.com>
2024-02-19 23:29:13 +0900

    [Feat] Interceptor pattern 관리 Properties 추가
    Related to: #15 #22

---
+++ src/main/java/com/woodada/common/config/InterceptorProperties.java
@@ -0,0 +1,30 @@
+package com.woodada.common.config;
+
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties("interceptor")
+public class InterceptorProperties {
+
+    private final Map<String, List<String>> addPatterns;
+    private final Map<String, List<String>> excludePatterns;
+
+    public InterceptorProperties(final Map<String, List<String>> addPatterns, final Map<String, List<String>> excludePatterns) {
+        this.addPatterns = addPatterns;
+        this.excludePatterns = excludePatterns;
+    }
+
+    public String[] getAuthExcludePatterns() {
+        final List<String> authExcludePatterns = excludePatterns.get("auth");
+        return authExcludePatterns.toArray(String[]::new);
+    }
+
+
+    public String[] getAuthAddPatterns() {
+        final List<String> authAddPatterns = addPatterns.get("auth");
+        return authAddPatterns.toArray(String[]::new);
+    }
+}
