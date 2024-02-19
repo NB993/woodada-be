06ca3adb1f58dd9a7ea90b0468e842e021668d3c
Jaehong Choe <qnalsfj@gmail.com>
2024-02-19 20:40:36 +0900

    [Remove] GetMeUseCase 관련 제거
    Related to: #15 #22

--- src/main/java/com/woodada/core/member/application/port/in/GetMeUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.woodada.core.member.application.port.in;
-
-import com.woodada.common.auth.argument_resolver.WddMember;
-import com.woodada.core.member.domain.WddMe;
-
-public interface GetMeUseCase {
-
-    WddMe getMe(WddMember wddMember);
-}
