bb61323fe1d920957b1bb79953b01e401c534f37
Jaehong Choe <qnalsfj@gmail.com>
2024-03-05 00:40:40 +0900

    [Test] TokenController 테스트를 SpringBootTest로 변경
    Related to: #15 #25

--- src/test/java/com/woodada/common/auth/adapter/in/TokenControllerTest.java
+++ src/test/java/com/woodada/common/auth/adapter/in/TokenControllerTest.java
@@ -3,44 +3,31 @@ package com.woodada.common.auth.adapter.in;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.woodada.common.auth.adapter.in.response.ReIssueTokenResponse;
-import com.woodada.common.auth.application.port.in.ReIssueTokenUseCase;
 import com.woodada.common.auth.argument_resolver.MemberHelper;
 import com.woodada.common.auth.argument_resolver.WddMember;
-import com.woodada.common.auth.domain.Token;
+import com.woodada.common.auth.domain.JwtHandler;
 import com.woodada.common.auth.domain.UserRole;
 import com.woodada.common.support.ApiResponse;
-import java.time.Instant;
+import com.woodada.test_helper.IntegrationTestBase;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-@ExtendWith(MockitoExtension.class)
 @DisplayName("TokenController 테스트")
-class TokenControllerTest {
+class TokenControllerTest extends IntegrationTestBase {
 
-    private ReIssueTokenController tokenController;
+    @Autowired private ReIssueTokenController tokenController;
+    @Autowired private JwtHandler jwtHandler;
 
-    @Mock private ReIssueTokenUseCase reIssueTokenUseCase;
-
-    @BeforeEach
-    void setUp() {
-        tokenController = new ReIssueTokenController(reIssueTokenUseCase);
-    }
-
+    @DisplayName("인증된 사용자 정보를 전달받으면 엑세스토큰과 리프레시 토큰을 재발급한다.")
     @Test
-    void success() {
+    void testReIssueToken() {
         //given
         WddMember wddMember = MemberHelper.createWddMember(1L, "test@email.com", "테스트유저", "test_profile_url", UserRole.NORMAL);
-        Mockito.when(reIssueTokenUseCase.reIssue(1L, Instant.now()))
-            .thenReturn(new Token("re_issued_access_token", "re_issued_refresh_token"));
 
         //when
         final ResponseEntity<ApiResponse<ReIssueTokenResponse>> responseEntity = tokenController.reIssueToken(wddMember);
@@ -52,9 +39,8 @@ class TokenControllerTest {
         Assertions.assertAll(
             () -> assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK),
             () -> assertThat(body.isSuccess()).isTrue(),
-            () -> assertThat(data.accessToken()).isEqualTo("re_issued_access_token"),
-            () -> assertThat(responseEntity.getHeaders().get(HttpHeaders.SET_COOKIE))
-                .anyMatch(cookie -> cookie.contains("re_issued_refresh_token"))
+            () -> assertThat(jwtHandler.decodeToken(data.accessToken())).isEqualTo(1L),
+            () -> assertThat(responseEntity.getHeaders().get(HttpHeaders.SET_COOKIE)).isNotEmpty()
         );
     }
 }
