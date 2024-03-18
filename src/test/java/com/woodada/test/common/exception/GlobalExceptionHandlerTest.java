4af98b470992f7db46fa8729f1143a6131ad0a5f
Jaehong Choe <qnalsfj@gmail.com>
2024-03-18 23:45:04 +0900

    [Test] 깨지는 테스트 수정
    - AuthInterceptor의 StringRedisTemplate 의존성이 생겨
    RestAssured로 변경
    
    Related to: #15 #25

--- src/test/java/com/woodada/test/common/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/com/woodada/test/common/exception/GlobalExceptionHandlerTest.java
@@ -1,153 +1,115 @@
 package com.woodada.test.common.exception;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.woodada.common.auth.adapter.out.persistence.MemberRepository;
-import com.woodada.common.auth.domain.JwtHandler;
-import com.woodada.common.auth.domain.JwtProperties;
-import com.woodada.common.config.CorsProperties;
-import com.woodada.common.config.InterceptorProperties;
-import com.woodada.common.exception.WddException;
 import com.woodada.test.common.exception.ExceptionTestController.TestRequestBody;
+import com.woodada.test_helper.AcceptanceTestBase;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
-//todo TestController에 목 객체 주입해서 예외 발생 메서드를 stubbing하는 방법도 고려.
-@DisplayName("[unit test] GlobalExceptionHandler 단위 테스트")
-@ActiveProfiles("test")
-@WebMvcTest(value = {ExceptionTestController.class, JwtHandler.class, JwtProperties.class, CorsProperties.class, InterceptorProperties.class})
-public class GlobalExceptionHandlerTest {
-
-    @Autowired private MockMvc mockMvc;
-    @Autowired private ObjectMapper objectMapper;
-
-    @MockBean private MemberRepository memberRepository;
+@DisplayName("[integration test] GlobalExceptionHandler 통합 테스트")
+public class GlobalExceptionHandlerTest extends AcceptanceTestBase {
 
     @DisplayName("IllegalArgumentException 핸들러 테스트")
     @Test
-    void illegal_argument_exception_response() throws Exception{
-        //when
-        final ResultActions perform = mockMvc.perform(post("/exception/illegal-argument-exception"));
-
-        //then
-        perform
-            .andExpect(status().isBadRequest())
-            .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(IllegalArgumentException.class))
-            .andExpect(jsonPath("success").value(false))
-            .andExpect(jsonPath("error.code").value("400"))
-            .andExpect(jsonPath("error.message").exists())
-            .andExpect(jsonPath("error.validations").isEmpty())
-            .andDo(print());
+    void illegal_argument_exception_ㄲresponse() {
+        when()
+            .post("/exception/illegal-argument-exception")
+        .then()
+            .statusCode(400)
+            .body("success", equalTo(false))
+            .body("error.code", equalTo("400"))
+            .body("error.message", notNullValue())
+            .body("error.validations", empty())
+            .log().all();
     }
-
+//
     @DisplayName("HttpRequestMethodNotSupportedException 핸들러 테스트")
     @Test
-    void http_request_method_not_supported_exception_response() throws Exception {
-        //when
-        final ResultActions perform = mockMvc.perform(
-            delete("/exception/http-request-method-not-supported-exception"));
-
-        //then
-        perform
-            .andExpect(status().isMethodNotAllowed())
-            .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(HttpRequestMethodNotSupportedException.class))
-            .andExpect(jsonPath("success").value(false))
-            .andExpect(jsonPath("error.code").value("405"))
-            .andExpect(jsonPath("error.validations").isEmpty())
-            .andDo(print());
+    void http_request_method_not_supported_exception_response() {
+        when()
+            .delete("/exception/http-request-method-not-supported-exception")
+        .then()
+            .statusCode(HttpStatus.METHOD_NOT_ALLOWED.value())
+            .body("success", equalTo(false))
+            .body("error.code", equalTo("405"))
+            .body("error.message", notNullValue())
+            .body("error.validations", empty())
+            .log().all();
     }
 
     @DisplayName("HttpMessageNotReadableException 핸들러 테스트")
     @Test
-    void http_message_not_readable_exception_response() throws Exception {
-        //when
-        ResultActions perform = mockMvc.perform(post("/exception/http-message-not-readable-exception")
-            .contentType(MediaType.APPLICATION_JSON));
+    void http_message_not_readable_exception_response() {
+        given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when()
+            .post("/exception/http-message-not-readable-exception")
             //body 입력 X
-
-        //then
-        perform
-            .andExpect(status().isBadRequest())
-            .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(HttpMessageNotReadableException.class))
-            .andExpect(jsonPath("success").value(false))
-            .andExpect(jsonPath("error.code").value("400"))
-            .andExpect(jsonPath("error.message").exists())
-            .andExpect(jsonPath("error.validations").isEmpty())
-            .andDo(print());
+        .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .body("success", equalTo(false))
+            .body("error.code", equalTo("400"))
+            .body("error.message", notNullValue())
+            .body("error.validations", empty())
+            .log().all();
     }
 
     @DisplayName("MethodArgumentNotValidException 핸들러 테스트")
     @Test
-    void method_argument_not_valid_exception_response() throws Exception {
-        //given
+    void method_argument_not_valid_exception_response() {
         final TestRequestBody invalidBody = new TestRequestBody("1234", -1, List.of());
 
-        //when
-        ResultActions perform = mockMvc.perform(post("/exception/method-argument-not-valid-exception")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(invalidBody)));
-
-        //then
-        perform
-            .andExpect(status().isBadRequest())
-            .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(MethodArgumentNotValidException.class))
-            .andExpect(jsonPath("success").value(false))
-            .andExpect(jsonPath("error.code").value("400"))
-            .andExpect(jsonPath("error.message").value("입력 조건을 위반하였습니다."))
-            .andExpect(jsonPath("error.validations").exists())
-            .andDo(print());
+        given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(invalidBody)
+        .when()
+            .post("/exception/http-message-not-readable-exception")
+            //body 입력 X
+        .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .body("success", equalTo(false))
+            .body("error.code", equalTo("400"))
+            .body("error.message", equalTo("입력 조건을 위반하였습니다."))
+            .body("error.validations", hasSize(greaterThan(0)))
+            .log().all();
     }
 
     @DisplayName("WddException 핸들러 테스트")
     @Test
-    void wdd_exception_response() throws Exception {
-        //when
-        ResultActions perform = mockMvc.perform(get("/exception/business-exception"));
-
-        //then
-        perform
-            .andExpect(status().isNotAcceptable())
-            .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(WddException.class))
-            .andExpect(jsonPath("success").value(false))
-            .andExpect(jsonPath("error.code").value(TestException.TEST.getCode()))
-            .andExpect(jsonPath("error.message").value(TestException.TEST.getMessage()))
-            .andExpect(jsonPath("error.validations").isEmpty())
-            .andDo(print());
+    void wdd_exception_response() {
+        when()
+            .get("/exception/business-exception")
+            //body 입력 X
+        .then()
+            .statusCode(TestException.TEST.getStatus().value())
+            .body("success", equalTo(false))
+            .body("error.code", equalTo(TestException.TEST.getCode()))
+            .body("error.message", equalTo(TestException.TEST.getMessage()))
+            .body("error.validations", empty())
+            .log().all();
     }
 
     @DisplayName("Exception 핸들러 테스트")
     @Test
-    void server_error_response() throws Exception {
-        //when
-        ResultActions perform = mockMvc.perform(get("/exception/internal-server-error"));
-
-        //then
-        perform
-            .andExpect(status().isInternalServerError())
-            .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(Exception.class))
-            .andExpect(jsonPath("success").value(false))
-            .andExpect(jsonPath("error.code").value("500"))
-            .andExpect(jsonPath("error.message").value("서버 에러"))
-            .andExpect(jsonPath("error.validations").isEmpty())
-            .andDo(print());
+    void server_error_response() {
+        when()
+            .get("/exception/internal-server-error")
+        .then()
+            .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            .body("success", equalTo(false))
+            .body("error.code", equalTo("500"))
+            .body("error.message", equalTo("서버 에러"))
+            .body("error.validations", empty())
+            .log().all();
     }
-
 }
