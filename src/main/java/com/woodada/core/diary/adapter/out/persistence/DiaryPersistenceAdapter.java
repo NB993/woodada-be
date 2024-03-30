e3f390c38e67da055290d8592f5af6be2ada4b80
Jaehong Choe <qnalsfj@gmail.com>
2024-03-30 17:56:03 +0900

    [Feat] #33 일기 조회 기능 추가

--- src/main/java/com/woodada/core/diary/adapter/out/persistence/DiaryPersistenceAdapter.java
+++ src/main/java/com/woodada/core/diary/adapter/out/persistence/DiaryPersistenceAdapter.java
@@ -5,6 +5,7 @@ import com.woodada.core.diary.application.port.out.DiarySavePort;
 import com.woodada.core.diary.domain.Diary;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,6 +20,17 @@ public class DiaryPersistenceAdapter implements DiaryFindPort, DiarySavePort {
     @Override
     public boolean existsDiary(final Long createdBy, final LocalDate writeDate) {
         return diaryRepository.existsByCreatedByAndCreatedAtBetween(createdBy, writeDate.atStartOfDay(), writeDate.atTime(LocalTime.MAX));
+    }
+
+    @Override
+    public Optional<Diary> findDiary(final Long id, final Long createdBy) {
+        final Optional<DiaryJpaEntity> optionalDiary = diaryRepository.findByIdAndCreatedBy(id, createdBy);
+        if (optionalDiary.isEmpty()) {
+            return Optional.empty();
+        }
+
+        final DiaryJpaEntity foundDiary = optionalDiary.get();
+        return Optional.of(foundDiary.toDomainEntity());
     }
 
     @Override
--- src/main/java/com/woodada/core/diary/application/port/out/DiaryFindPort.java
+++ src/main/java/com/woodada/core/diary/application/port/out/DiaryFindPort.java
@@ -1,6 +1,8 @@
 package com.woodada.core.diary.application.port.out;
 
+import com.woodada.core.diary.domain.Diary;
 import java.time.LocalDate;
+import java.util.Optional;
 
 public interface DiaryFindPort {
 
@@ -12,4 +14,13 @@ public interface DiaryFindPort {
      * @return 일기 존재 여부
      */
     boolean existsDiary(Long createdBy, LocalDate writeDate);
+
+    /**
+     * 일기를 조회한다.
+     *
+     * @param id        일기 id
+     * @param createdBy 작성자 id
+     * @return 일기
+     */
+    Optional<Diary> findDiary(Long id, Long createdBy);
 }
