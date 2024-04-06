4446fa3675396ba20958b22cd33eb9c834dc1808
Jaehong Choe <qnalsfj@gmail.com>
2024-04-06 17:42:41 +0900

    [Feat] #33 AuditAware 임시 처리
    todo
    - 다이어리 crud 끝난 후 spring security 적용하면서 setter 제거

--- src/main/java/com/woodada/common/config/jpa/AuditorAwareImpl.java
+++ src/main/java/com/woodada/common/config/jpa/AuditorAwareImpl.java
@@ -5,9 +5,16 @@ import org.springframework.data.domain.AuditorAware;
 
 public class AuditorAwareImpl implements AuditorAware<Long> {
 
+    private Long memberId = 0L;
+
     @Override
     public Optional<Long> getCurrentAuditor() {
         //todo spring security
-        return Optional.of(0L);
+        return Optional.of(memberId);
+    }
+
+    //todo 다이어리 crud 끝내고 시큐리티 적용..
+    public void setCurrentAuditor(Long memberId) {
+        this.memberId = memberId;
     }
 }
