9b304ca5fb45f39ae45249e97de0d998afc4d7b8
Jaehong Choe <qnalsfj@gmail.com>
2024-04-10 14:23:41 +0900

    [Test] #36 Audit cleanUp 코드 추가
    - 인수테스트에서 토큰 정보를 이용해 auditor가 set 되고 있다보니
    auditor 정보를 사용하지 못하는 영속성 테스트에서 cleanUp 되지 않은
    auditor id 때문에 조회에 실패하는 케이스가 발생하여 이를 수정함

--- src/test/java/com/woodada/test_helper/AcceptanceTestBase.java
+++ src/test/java/com/woodada/test_helper/AcceptanceTestBase.java
@@ -1,12 +1,14 @@
 package com.woodada.test_helper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.woodada.common.config.jpa.AuditorAwareImpl;
 import com.woodada.test_helper.annotation.IntegrationTest;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.data.domain.AuditorAware;
 
 @IntegrationTest
 public abstract class AcceptanceTestBase {
@@ -15,6 +17,7 @@ public abstract class AcceptanceTestBase {
 
     @Autowired private RDBInitialization rdbInitialization;
     @Autowired private RedisInitialization redisInitialization;
+    @Autowired private AuditorAware<Long> auditorAware;
 
     protected final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -28,5 +31,6 @@ public abstract class AcceptanceTestBase {
     void cleanUp() {
         rdbInitialization.truncateAllEntity();
         redisInitialization.initRedis();
+        ((AuditorAwareImpl) auditorAware).setCurrentAuditor(0L); // todo 다이어리 CRUD 끝나고 변경..
     }
 }
