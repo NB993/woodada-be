c47d32187e730abb6fa541d5d248d502f359a066
Jaehong Choe <qnalsfj@gmail.com>
2024-04-10 13:23:39 +0900

    [Feat, Test] #36 일기 조회 컨트롤러 마무리, 인수테스트 추가

--- src/main/java/com/woodada/core/diary/adapter/in/DiaryQueryAdapter.java
+++ src/main/java/com/woodada/core/diary/adapter/in/DiaryQueryAdapter.java
@@ -5,9 +5,13 @@ import com.woodada.common.support.ApiResponse;
 import com.woodada.core.diary.adapter.in.response.DiaryQueryResponse;
 import com.woodada.core.diary.application.port.in.DiaryQueryCommand;
 import com.woodada.core.diary.application.port.in.DiaryQueryUseCase;
+import com.woodada.core.diary.domain.Diary;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,13 +26,19 @@ public class DiaryQueryAdapter {
         this.diaryQueryUseCase = diaryQueryUseCase;
     }
 
-    ResponseEntity<ApiResponse<DiaryQueryResponse>> queryDiaries(
+    @GetMapping
+    ResponseEntity<ApiResponse<List<DiaryQueryResponse>>> queryDiaries(
         WddMember wddMember,
         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
     ) {
         final DiaryQueryCommand command = new DiaryQueryCommand(startDate, endDate);
+        final List<Diary> diaries = diaryQueryUseCase.queryDiaries(wddMember, command);
+        final List<DiaryQueryResponse> response = diaries.stream()
+            .map(DiaryQueryResponse::from)
+            .collect(Collectors.toList());
 
-        return null;
+        final ApiResponse<List<DiaryQueryResponse>> apiResponse = ApiResponse.success(response);
+        return ResponseEntity.ok(apiResponse);
     }
 }
--- src/main/java/com/woodada/core/diary/adapter/in/response/DiaryQueryResponse.java
+++ src/main/java/com/woodada/core/diary/adapter/in/response/DiaryQueryResponse.java
@@ -1,12 +1,17 @@
 package com.woodada.core.diary.adapter.in.response;
 
+import com.woodada.core.diary.domain.Diary;
 import java.time.LocalDateTime;
 
 public record DiaryQueryResponse(
     Long id,
     String title,
     String contents,
-    LocalDateTime createdAt
+    LocalDateTime createdAt,
+    LocalDateTime modifiedAt
 ) {
 
+    public static DiaryQueryResponse from(final Diary diary) {
+        return new DiaryQueryResponse(diary.getId(), diary.getTitle(), diary.getContents(), diary.getCreatedAt(), diary.getModifiedAt());
+    }
 }
